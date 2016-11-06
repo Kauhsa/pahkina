0670ae41c2dd5eb56cd8ec2252002c91bcddbdea
Kauhsa <kauhsa@kapsi.fi>
2016-11-06 18:40:42 +0200

    Clean up parseHourEntryCSV

--- src/hourEntry/parseHourEntryCSV.ts
+++ src/hourEntry/parseHourEntryCSV.ts
@@ -43,53 +43,81 @@ class ParseError {
   }
 }
 
-function parseHourEntryRow(row: string[]): HourEntry | ParseError {
-  if (row.length != 5) {
-    return new ParseError("invalid-columns", "Row should have 5 columns, had " + row.length);
-  }
+type PossibleError<T> = T | ParseError;
 
-  const strippedRow = row.map(s => s.trim())
-  const [nameToken, idToken, dateToken, startToken, endToken] = strippedRow;
-
-  // validate name
+function parseName(nameToken: string): PossibleError<string> {
   if (nameToken.length === 0) {
     return new ParseError("invalid-name", "Name cannot be empty")
   }
 
-  // validate id
+  return nameToken;
+}
+
+function parseId(idToken: string): PossibleError<number> {
   if (!idToken.match(/^\d+$/)) {
     return new ParseError("invalid-id", "Id '" + idToken + "' is invalid")
   }
-  const id = Number(idToken);
 
-  // parse date
+  return Number(idToken);
+}
+
+function parseDate(dateToken: string): PossibleError<moment.Moment> {
   const validDateFormats = ["DD.MM.YYYY", "D.MM.YYYY", "DD.M.YYYY", "D.M.YYYY"]
   const date = moment(dateToken, validDateFormats, true);
+
   if (!date.isValid()) {
     return new ParseError("invalid-date", "Date '" + dateToken + "' is invalid")
   }
 
-  // parse start time
-  const startTime = Time.fromString(startToken)
+  return date;
+}
+
+function parseTime(timeToken: string): PossibleError<Time> {
+  const startTime = Time.fromString(timeToken)
+
   if (startTime == null) {
-    return new ParseError("invalid-time", "Time '" + startToken + "' is invalid")
+    return new ParseError("invalid-time", "Time '" + timeToken + "' is invalid")
   }
 
-  // parse end time
-  const endTime = Time.fromString(endToken)
-  if (endTime == null) {
-    return new ParseError("invalid-time", "Time '" + endToken + "' is invalid")
+  return startTime;
+}
+
+function parseHourEntryRow(row: string[]): PossibleError<HourEntry> {
+  if (row.length != 5) {
+    return new ParseError("invalid-columns", "Row should have 5 columns, had " + row.length);
   }
 
-  const startDate = date.clone().hours(startTime.hours).minutes(startTime.minutes)
-  const endDate = date.clone().hours(endTime.hours).minutes(endTime.minutes)
+  const [nameToken, idToken, dateToken, startToken, endToken] = row.map(s => s.trim());
+
+  const name = parseName(nameToken);
+  const id = parseId(idToken);
+  const date = parseDate(dateToken);
+  const start = parseTime(startToken);
+  const end = parseTime(endToken);
+
+  // [name, id, date, start, end].some(x => x instanceof ParseError) would be
+  // cute, but type guard does not work then :(
+  if (name instanceof ParseError) {
+    return name;
+  } else if (id instanceof ParseError) {
+    return id;
+  } else if (date instanceof ParseError) {
+    return date;
+  } else if (start instanceof ParseError) {
+    return start;
+  } else if (end instanceof ParseError) {
+    return end;
+  }
+
+  const startDate = start.momentDateOnTime(date)
+  const endDate = end.momentDateOnTime(date)
 
   // is the entry ending on the following day?
   if (endDate.isBefore(startDate)) {
     endDate.add(1, "days")
   }
 
-  return new HourEntry(nameToken, id, startDate, endDate)
+  return new HourEntry(name, id, startDate, endDate)
 }
 
 export default function parseHourEntryCSV(data: string): ParseResult {
